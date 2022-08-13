035497e3b2c4e094a02a26620744e48d03f5504e
Mohammed Achraf LEBRAZI <mohammedachraf.lebrazi@here.com>
2022-08-13 18:39:38 +0200

    Added reactjs client app

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            I am some other page!
+            <link to="/"> Go back home! </link>
+        </div>
+    );
+};
