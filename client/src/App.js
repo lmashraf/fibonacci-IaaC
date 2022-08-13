c4094df2454a3b2393ac2f132741546e01d6f610
Mohammed Achraf LEBRAZI <mohammedachraf.lebrazi@here.com>
2022-08-13 22:13:58 +0200

    Added Fib.js content and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowseRouter as Router, Route, Link } from 'react-router-dom';
+
+import Fib from './Fib'
+import OtherPage from './OtherPage';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
+
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">OtherPage</Link>
       </header>
+      <div>
+        <Route exact path="/" component={Fib}/>
+        <Route exact path="/otherpage" component={OtherPage}/>
+      </div>
     </div>
+    </Router>
   );
 }
 
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -22,5 +22,53 @@ class Fib extends Component {
         const seenIndexes = await axios.get('/api/values/all');
         this.setState({seenIndexes: seenIndexes.data});
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values',{
+            index: this.state.index
+        });
+        this.setState({ index: '' });
+    };
+
+    renderSeenIndex() {
+        return this.state.seenIndexes.map(({ number }) => number).join (', ');
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    Fibonacci({key}) = {this.state.values[{key}]}
+                </div>
+            );
+        }
+        return this.state.values;
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Enter your index:
+                        <input value={this.state.index}
+                        onChange={event => this.setState({index: event.target.value})}
+                        />
+                        <button>Submit</button>
+                    </label>
+                </form>
+
+                <h3>Indexes seen:</h3>
+                {this.renderSeenIndexes()}
+
+                <h3>Calculated values:</h3>
+                {this.renderValues()}
+            </div>
+        )
+    }
 };
 
+export default Fib;
