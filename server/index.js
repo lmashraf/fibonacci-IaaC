ff94f2da8f419b0355f2804a6c68d1c895d45f5c
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-08-14 14:54:56 +0200

    Reverting changes from dependencies due to conflicts. Added WS fix to nginx

--- server/index.js
+++ server/index.js
@@ -1,77 +1,70 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
-// Express App
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-
-app.use(cors());;
+app.use(cors());
 app.use(bodyParser.json());
 
-// Postgress Client
-const { Pool } = require('pg');
-
+// Postgres Client Setup
+const { Pool } = require("pg");
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
-pgClient.on('connect', (client) => {
-    client
-        .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-        .catch((err) => console.error(err));
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
 });
 
-// Redis Client
-const redis = require('redis');
+// Redis Client Setup
+const redis = require("redis");
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000 // 1 second
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
-app.get('/', (req, res) => {
-    res.send('Hi');
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 
-app.get('values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * FROM VALUES');
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
 
-    res.send(values.rows);
+  res.send(values.rows);
 });
 
-app.get('values/current', async (req, res) => {
-    redisClient.hGetAll('values', (err, values) => {
-        res.send(values);
-    });
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
 });
 
-app.post('/values', async (req, res) => {
-    const index = req.body.index;
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
 
-    if (parseInt(index) > 40) {
-        return res.status(422).send('Index too high!');
-    }
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
 
-    redisClient.hSet('values', index, 'Nothing yet!');
-    redisPublisher.publish('insert', index);
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
 
-    // Permanently store in Postgres database
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
-
-    res.send({
-        working: true
-    });
+  res.send({ working: true });
 });
 
-app.listen(5000, err => {
-    console.log('Listening on port 5000..')
+app.listen(5000, (err) => {
+  console.log("Listening");
 });
--- worker/index.js
+++ worker/index.js
@@ -3,13 +3,13 @@ const keys = require('./keys');
 
 // Create a Redis client
 const redis = require('redis');
-const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    // Automatically reconnect every 1 second (in ms)
-    retry_strategy: () => 1000
-});
 
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  // Automatically reconnect every 1 second (in ms)  
+  retry_strategy: () => 1000,
+});
 // Duplicate Redis client
 const sub = redisClient.duplicate();
 
@@ -17,16 +17,13 @@ const sub = redisClient.duplicate();
 // Purposely recursive, because it is slow and justifies
 // the use of a separate worker process and Redis.
 function fib(index) {
-    if(index < 2) {
-        return 1;
-    }
-
-    return fib(index - 1) + fib(index -2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
 // For each N value on Redis, calculate fib(N)
 // and store it on a hash.
 sub.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
