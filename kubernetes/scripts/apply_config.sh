5ce47332624633821038594db6648edda904a0d7
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-08-18 09:06:55 +0200

    Added Redis server k8s config files

--- kubernetes/scripts/apply_config.sh
+++ kubernetes/scripts/apply_config.sh
@@ -11,3 +11,7 @@ kubectl apply -f ../server/
 # Worker
 echo "- Applying Client k8s configuration ..."
 kubectl apply -f ../worker/
+
+# Redis
+echo "- Applying Redis k8s configuration ..."
+kubectl apply -f ../redis/
